7497823be41e0cf53b95f33660a6e5634242af28
maxleiko <max.tricoire@gmail.com>
2017-11-20 15:11:37 +0100

    for now, generate configs in /tmp

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const tmp = require('tmp');
 const yeoman = require('yeoman-environment');
 const TerminalAdapter = require('./lib/TerminalAdapter');
 
@@ -6,27 +7,18 @@ process.on('unhandledRejection', (err) => {
   process.exit(1);
 });
 
+const tmpDir = tmp.dirSync();
+tmpDir.removeCallback();
+console.log('Generating in:', tmpDir.name);
+
+const args = [];
+const opts = {
+  cwd: tmpDir.name
+};
 const model = {};
-const env = yeoman.createEnv([], {}, new TerminalAdapter(model));
+const env = yeoman.createEnv(args, opts, new TerminalAdapter(model));
 env.register('./node_modules/generator-jhipster/generators/app/index.js', 'jhipster');
 
-env.on('prompting', () => {
-  console.log();
-  console.log('=======> PROMPTING');
-  console.log();
-});
-
-env.on('writing', () => {
-  console.log();
-  console.log('=======> WRITING');
-  console.log();
-  process.exit(0);
-});
-
-env.on('end', () => {
-  console.log();
-  console.log('=======> END');
-  console.log();
-});
+// TODO quit before the writing is done
 
 env.run('jhipster');
