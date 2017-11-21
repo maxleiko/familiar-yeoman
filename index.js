2c8957405c718e7a53df03edce1ba54d147b7130
maxleiko <max.tricoire@gmail.com>
2017-11-21 17:33:25 +0100

    update gen workflow

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-const tmp = require('tmp');
-const yeoman = require('yeoman-environment');
-const TerminalAdapter = require('./lib/TerminalAdapter');
-
 process.on('unhandledRejection', (err) => {
   console.error(err.stack);
   process.exit(1);
 });
 
-const tmpDir = tmp.dirSync();
-console.log('Generating in:', tmpDir.name);
+const generate = require('./lib/generate');
 
-const args = [];
-const options = { cwd: tmpDir.name };
 const model = {};
-const env = yeoman.createEnv(args, options, new TerminalAdapter(model));
-env.on('error', (err) => {
-  console.error(err.stack);
-  process.exit(1);
-});
-
-env.register(require.resolve('generator-jhipster/generators/app'));
-
-env.run('jhipster', () => {
-  console.log('Done');
+Promise.all([
+  generate(model),
+  // generate(model),
+]).then((configs) => {
+  console.log(configs);
 });
